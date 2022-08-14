215a614c1c6ecc02b6fc8056926dbc9227bf35f0
althafdemiandra <100656575+althafdemiandra@users.noreply.github.com>
2022-08-14 11:30:01 +0700

    feat: create homepage

--- src/layouts/Navigation/index.tsx
+++ src/layouts/Navigation/index.tsx
@@ -1,9 +1,50 @@
+import ChatIcon from '@/assets/icons/ChatIcon';
+import { Box, Text } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
 
 interface NavigationProps {}
 
-const Navigation: React.FC<NavigationProps> = (props) => {
-  return <div>Navigation</div>;
+const Navigation: React.FC<NavigationProps> = () => {
+  return (
+    <Box
+      position={'absolute'}
+      bottom={0}
+      minW={'100%'}
+      bgColor={'green.600'}
+      display={'flex'}
+      alignItems="center"
+      justifyContent={'space-between'}
+      flexShrink={1}
+      px={'1rem'}
+      py="0.5rem"
+    >
+      <Box display={'flex'} flexDir={'column'} alignItems="center">
+        <ChatIcon />
+        <Text fontSize={'xs'} color="green.100">
+          Following
+        </Text>
+      </Box>
+
+      <Box display={'flex'} flexDir={'column'} alignItems="center">
+        <ChatIcon />
+        <Text fontSize={'xs'} color="green.100">
+          Messages
+        </Text>
+      </Box>
+
+      <Link href={'/profile'} passHref>
+        <a>
+          <Box display={'flex'} flexDir={'column'} alignItems="center">
+            <ChatIcon />
+            <Text fontSize={'xs'} color="green.100">
+              Profile
+            </Text>
+          </Box>
+        </a>
+      </Link>
+    </Box>
+  );
 };
 
 export default Navigation;
--- src/layouts/Wrapper/index.tsx
+++ src/layouts/Wrapper/index.tsx
@@ -1,14 +1,22 @@
-import { Center, Container } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import React, { FC, ReactNode } from 'react';
+import Navigation from '../Navigation';
 
 type Props = {
   children: ReactNode;
 };
 
 const Wrapper: FC<Props> = ({ children }) => {
+  const router = useRouter();
+  const hideNavigation = ['/', '/login', '/register'].some(
+    (item) => item === router.asPath
+  );
+
   return (
-    <Center>
-      <Container>{children}</Container>
+    <Center flexDir={'column'} position={'relative'} minH={'100vh'}>
+      {children}
+      {!hideNavigation && <Navigation />}
     </Center>
   );
 };
