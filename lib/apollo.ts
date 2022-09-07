349adc2534730dff473b5b408540daf3d9f9500c
althafdemiandra <92288572+althafdaa@users.noreply.github.com>
2022-09-07 20:55:48 +0700

    fix: adjust for deployment

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -27,7 +27,7 @@ function createIsomorphicLink(ctx?: SchemaContext) {
     });
   }
   const httpLink = new HttpLink({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: '/api/graphql',
     credentials: 'same-origin',
   });
   return from([httpLink]);
