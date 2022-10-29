71252f279a7fee2aaead2e5a2d37a6c7183c44a3
althafdemiandra <althafdemiandra@gmail.com>
2022-10-29 07:27:33 +0700

    test new page

---
+++ src/pages/new-test/index.jsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface NewTestPageProps {}
+
+const NewTestPage: FC<NewTestPageProps> = (props) => {
+  return <div>NewTestPage</div>;
+};
+
+export default NewTestPage;
